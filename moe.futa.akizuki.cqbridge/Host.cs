141178390c8c4a94257dc0ccb77ac3c2d4cfae10
Naoki Rinmous <sukareki@gmail.com>
2017-07-28 12:53:42 +0800

    Initial commit

---
+++ moe.futa.akizuki.cqbridge/Host.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace moe.futa.akizuki.cqbridge
+{
+    public class HostLogLevel
+    {
+        public const Int32 DEBUG = 0;
+        public const Int32 INFO = 10;
+        public const Int32 INFO_SUCCESS = 11;
+        public const Int32 INFO_RECV = 12;
+        public const Int32 INFO_SEND = 13;
+        public const Int32 WARNING = 20;
+        public const Int32 ERROR = 30;
+        public const Int32 FATAL = 40;
+    }
+
+    public class Host
+    {
+        private const String LIBRARY_NAME = "cqp.dll";
+
+        // CQAPI(int32_t) CQ_addLog(int32_t AuthCode, int32_t priority, const char* category, const char* content);
+        [DllImport(LIBRARY_NAME, CallingConvention = CallingConvention.StdCall, EntryPoint = "CQ_addLog")]
+        public static extern Int32 AppendLog(Int32 authCode, Int32 priority, String category, String content);
+    }
+}
---
+++ moe.futa.akizuki.cqbridge/HostClient.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace moe.futa.akizuki.cqbridge
+{
+    public class HostClient
+    {
+        private const String APPID = "moe.futa.akizuki.cqbridge";
+
+        private const Int32 EVENT_IGNORED = 0;
+        private const Int32 EVENT_BLOCK = 1;
+
+        public static Int32 AuthCode;
+
+        // CQEVENT(const char*, AppInfo, 0)()
+        [DllExport("AppInfo", CallingConvention.StdCall)]
+        public static String GetAppInfo()
+        {
+            return "9," + APPID;
+        }
+
+        // CQEVENT(int32_t, __eventStartup, 0)()
+        [DllExport("OnHostStartup", CallingConvention.StdCall)]
+        public static Int32 Initialize()
+        {
+            Host.AppendLog(AuthCode, HostLogLevel.INFO, typeof(HostClient).Name, "Akizuki.CQBridge initialized.");
+            return 0;
+        }
+
+        // CQEVENT(int32_t, Initialize, 4)(int32_t AuthCode)
+        [DllExport("Initialize", CallingConvention.StdCall)]
+        public static Int32 SetAuthCode(Int32 authCode)
+        {
+            AuthCode = authCode;
+            return 0;
+        }
+    }
+}
