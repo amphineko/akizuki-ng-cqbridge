5c357f220e0d6c42c45356f7fd9a60781c9560eb
Naoki Rinmous <sukareki@gmail.com>
2017-08-03 02:50:38 +0800

    Implemented configuration store

---
+++ moe.futa.akizuki.cqbridge/Configuration.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using Nett;
+
+namespace moe.futa.akizuki.cqbridge
+{
+    internal class Configuration
+    {
+        private static Configuration _instance;
+
+        public static Configuration GetInstance()
+        {
+            return _instance ?? (_instance = Toml.ReadFile<Configuration>(Path.Combine(
+                       Host.GetHostPath(HostClient.AuthCode), "config.toml")));
+        }
+    }
+}
--- moe.futa.akizuki.cqbridge/HostClient.cs
+++ moe.futa.akizuki.cqbridge/HostClient.cs
@@ -41,6 +41,16 @@ namespace moe.futa.akizuki.cqbridge
         public static Int32 Initialize()
         {
             Host.AppendLog(AuthCode, HostLogLevel.Info, typeof(HostClient).Name, "Akizuki.CQBridge initializing.");
+            Configuration config;
+            try
+            {
+                config = Configuration.GetInstance();
+            }
+            catch (Exception e)
+            {
+                Host.AppendLog(AuthCode, HostLogLevel.Fatal, typeof(HostClient).Name, e.Message);
+                return 0;
+            }
             _inbound = new InboundServer("tcp://0.0.0.0:11452");
             _outbound = new OutboundServer("tcp://0.0.0.0:11451");
             _startupTime = DateTime.Now;
